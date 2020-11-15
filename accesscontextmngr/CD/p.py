67f30d2148114a60b4b137ed9344e34d0c32e88f
Hermann Wagner <hermann.wagner01@gmail.com>
2020-11-15 10:41:51 +0000

    finished renderer

--- accesscontextmngr/CD/p.py
+++ accesscontextmngr/CD/p.py
@@ -7,6 +7,7 @@ from os.path import isfile, join, exists
 # optional args
 desc = ""
 cF = ""
+cmds = ["#!/bin/bash"]
 
 # create new folder for rendered yaml files
 newpath = './rendered'
@@ -20,30 +21,22 @@ files = [f for f in listdir(path) if isfile(join(path, f))]
 # only iterate through yaml files
 for f in files:
     if "yaml" == f.split(".")[-1]:
-        print(f)
-
         with open(f, 'r') as yaml_file:
             try:
                 data = yaml.safe_load(yaml_file)
                 for item, doc in data.items():
                     if item == "name":
-                        # print("name:", doc)
                         name = doc
                         filename = newpath + "/" + name.split("/")[3] + ".yaml"
-                        # print("fn:", filename)
                     if item == "description":
-                        # print("description:", doc)
                         desc = "--description='" + doc + "'"
                     if item == "title":
-                        # print("title:", doc)
                         title = doc
                     if item == "basic":
                         for k, v in doc.items():
                             if k == "combiningFunction":
-                                # print("cF:", v)
                                 cF = "--combine-function=" + v
                             if k == "conditions":
-                                # print("conditions:", v)
                                 cond = v
                     if item == "advanced":
                         print("not implemented yet")
@@ -52,8 +45,12 @@ for f in files:
                 with open(filename, "w", encoding="utf8") as outfile:
                     yaml.dump(cond, outfile, default_flow_style=False, allow_unicode=True)    
 
-                # return gcloud commands
-                print(f"gcloud access-context-manager levels $ACTION {name} --basic-level-spec={filename} {cF} --title={title} {desc}")
+                # save gcloud command
+                cmds.append(f"gcloud access-context-manager levels $ACTION {name} --basic-level-spec={filename} {cF} --title={title} {desc}")
 
             except yaml.YAMLError as exc:
                print(exc)
+
+with open("create_update_levels.sh", "w", encoding="utf8") as outfile:
+    for cmd in cmds:
+        outfile.write("%s\n\n" % cmd)
